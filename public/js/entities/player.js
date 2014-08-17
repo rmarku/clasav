2540fb81c789d6d32ba590d696f8132dc0665a8e
Ricardo Martin Marcucci <rmarku@gmail.com>
2014-08-17 20:15:53 -0300

    Implementacion basica del Juego con colocacion de ropa y MockUP del servidor, Tarea #8 terminada @5h

--- public/js/entities/player.js
+++ public/js/entities/player.js
@@ -1,223 +1,119 @@
-// Player entities - both for the local player as well as the 
-// network player
-/* ----------------------------------------------------------------- */
-game.NetworkPlayerEntity = me.CollectableEntity.extend({
-  init: function (x, y, settings) {
-    this.parent(x, y, settings);
-    this.gravity = 0;
-    this.step = 0;
-    this.id = settings.id;
-    this.health = 3;
+/************************************************************************************/
+/*                                                                                  */
+/*        a player entity                                                           */
+/*                                                                                  */
+/** ********************************************************************************* */
+game.PlayerEntity = me.Entity.extend({
 
-    this.isCollidable = true;
-    this.type = game.ENEMY_OBJECT;
+	init : function(x, y, settings) {
+		this._super(me.Entity, 'init', [ x, y, settings ]);
+		this.data = settings.data;
+		this.alwaysUpdate = true;
 
-    this.renderable.addAnimation('run-down', [0, 4, 8, 12], 100);
-    this.renderable.addAnimation('run-left', [1, 5, 9, 13], 100);
-    this.renderable.addAnimation('run-up', [2, 6, 10, 14], 100);
-    this.renderable.addAnimation('run-right', [3, 7, 11, 15], 100);
-    this.renderable.addAnimation('hit', [0, 1, 2, 3], 100);
-    this.renderable.setCurrentAnimation('run-down');
-    // set the default horizontal & vertical speed (accel vector)
-    //this.setVelocity(4, 4); //Not needed .. we do not do client predicion yet.
-    this.state = {};
-    this.lastAnimationUsed = "run-down"; // Needed to prevent animation timer to be resett.. wich seam to happen otherwise
-    this.animationToUseThisFrame = "run-down"; // Needed to prevent animation timer to be resett.. wich seam to happen otherwise
-  },
+		this.body.setVelocity(3, 3);
+		this.body.setFriction(0.3, 0.3);
+		// this.body.setMaxVelocity(4, 4);
+		this.body.gravity = 0;
 
-  update: function (dt) {
-    this.vel.x = 0;
-    this.vel.y = 0;
-    if (!Object.keys(this.state).length) {
-      return false;
-    }
-    /*
-    if(this.state['ghost']) {
-        var ghost = this;
-        ghost.renderable.alpha = 0.25;
-        ghost.invincible = true;
-        setTimeout(function() { 
-            ghost.renderable.alpha = 1; 
-            ghost.invincible = false; 
-        }, 1500);
-    }
-    */
+		this.vestir();
+		me.input.bindKey(me.input.KEY.LEFT, 'left');
+		me.input.bindKey(me.input.KEY.A, 'left');
+		me.input.bindKey(me.input.KEY.RIGHT, 'right');
+		me.input.bindKey(me.input.KEY.D, 'right');
+		me.input.bindKey(me.input.KEY.UP, 'up');
+		me.input.bindKey(me.input.KEY.W, 'up');
+		me.input.bindKey(me.input.KEY.DOWN, 'down');
+		me.input.bindKey(me.input.KEY.S, 'down');
 
-    if (this.state['left']) {
-      this.animationToUseThisFrame = 'run-left';
-    }
+		me.game.viewport.follow(this, me.game.viewport.AXIS.BOTH);
 
-    if (this.state['right']) {
-      this.animationToUseThisFrame = 'run-right';
-    }
+		// bounding
+		// this.addShape(new me.Rect(new me.Vector2d(7,10), 32, 32));
 
-    if (this.state['up']) {
-      this.animationToUseThisFrame = 'run-up';
-    }
+		this.isCollidable = true;
+		this.type = game.MAIN_PLAYER_OBJECT;
 
-    if (this.state['down']) {
-      this.animationToUseThisFrame = 'run-down';
-    }
+		this.renderable.addAnimation('run-down', [ 0, 1, 2, 3 ], 100);
+		this.renderable.addAnimation('run-left', [ 4, 5, 6, 7 ], 100);
+		this.renderable.addAnimation('run-right', [ 8, 9, 10, 11 ], 100);
+		this.renderable.addAnimation('run-up', [ 12, 13, 14, 15 ], 100);
+		this.renderable.setCurrentAnimation('run-down');
+		this.lastAnimationUsed = 'run-down';
+		this.animationToUseThisFrame = 'run-down';
 
-    if (this.animationToUseThisFrame != this.lastAnimationUsed) {
-      this.lastAnimationUsed = this.animationToUseThisFrame;
-      this.renderable.setCurrentAnimation(this.animationToUseThisFrame);
-    }
+		// this.body.addShape(new me.Rect(0, 0, this.body.width,
+		// this.body.height));
 
-    this.state = {};
-    this.parent(dt);
-    return true;
-  }
-});
+		// set the renderable position to bottom center
+		this.anchorPoint.set(0.5, 0.5);
 
-game.PlayerEntity = me.ObjectEntity.extend({
-  init: function (x, y, settings) {
+	},
+	update : function(dt) {
 
-    //console.log(settings.image);
-    this.parent(x, y, settings);
+		if (me.input.isKeyPressed('left')) {
+			this.animationToUseThisFrame = 'run-left';
+			this.body.vel.x -= this.body.accel.x * me.timer.tick;
+		}
 
-    //bounding
-    this.addShape(new me.Rect(new me.Vector2d(7,10), 32, 32));
-    
-    this.gravity = 0;
-    this.isWeaponCoolDown = false;
-    this.weaponCoolDownTime = 500;
+		if (me.input.isKeyPressed('right')) {
+			this.animationToUseThisFrame = 'run-right';
+			this.body.vel.x += this.body.accel.x * me.timer.tick;
 
-    // set up health (top 3, bottom 0)
-    this.health = 3;
-    this.id = settings.id;
+		}
+		if (me.input.isKeyPressed('up')) {
+			this.animationToUseThisFrame = 'run-up';
+			this.body.vel.y -= this.body.accel.y * me.timer.tick;
+		}
 
-    // set up multiplayer
-    this.isMP = settings.isMP;
-    this.step = 0;
+		if (me.input.isKeyPressed('down')) {
+			this.animationToUseThisFrame = 'run-down';
+			this.body.vel.y += this.body.accel.y * me.timer.tick;
+		}
+		if (this.animationToUseThisFrame != this.lastAnimationUsed) {
+			this.lastAnimationUsed = this.animationToUseThisFrame;
+			this.renderable.setCurrentAnimation(this.animationToUseThisFrame);
+		}
 
-    // set up mouseCoordinates
-    game.mouseTarget = { x: 0, y: 0 };
+		if (this.body.vel.length() > this.body.maxVel.x) {
+			// Now calc actual vel to prevent speeding by going diag..
+			this.body.vel.normalize();
+			this.body.vel.scale(this.body.maxVel.x);
+		}
+		this.body.update();
 
-    this.isCollidable = true;
-    this.type = game.MAIN_PLAYER_OBJECT;
-    this.state = {up:false, down:false, right:false, left:false}; //Only used to tell other clients what anim to use right now but will be reworked to tell server and other clients how to simulate and do predictions in simulation
-    this.stateChanged = false;
-    this.lastAnimationUsed = "run-down"; // Needed to prevent animation timer to be resett.. wich seam to happen otherwise
-    this.animationToUseThisFrame = "run-down"; // Needed to prevent animation timer to be resett.. wich seam to happen otherwise
-    
-    //this.setVelocity(4, 4); Not need t separate x and y forces.. since top-down game
-    this.accelForce = 4;
-    
+		if (this.body.vel.x != 0 || this.body.vel.y != 0
+				|| (this.renderable && this.renderable.isFlickering())) {
+			this._super(me.Entity, 'update', [ dt ]);
+			return true;
+		}
 
-    this.renderable.addAnimation('run-down', [0, 1, 2, 3], 100);
-    this.renderable.addAnimation('run-left', [4, 5, 6, 7], 100);
-    this.renderable.addAnimation('run-up', [8, 9, 10, 11], 100);
-    this.renderable.addAnimation('run-right', [12, 13, 14, 15], 100);
-    this.renderable.addAnimation('hit', [0, 4, 8, 12], 100);
-    this.renderable.setCurrentAnimation('run-down');
-    // set the default horizontal & vertical speed (accel vector)
-    
-    
-    
-    this.maxVel.x = this.maxVel.y =  25; //Slow fps can force bigger steps .. will find out how to override this since I plan to ad more physicslike sim later
+		return false;
+	},
+	vestir : function() {
+		this.canvas = document.createElement('canvas');
+		this.canvas.width = this.renderable.image.width;
+		this.canvas.height = this.renderable.image.height;
 
-    me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
-  },
-  update: function (dt) {
-    this.vel.x = 0;
-    this.vel.y = 0;
-    this.g_dt = dt /20 ; //To be able to keep original values of velocity but still use dt.
-    /*
-    if (this.state['ghost']) {
-      var ghost = this;
-      ghost.renderable.alpha = 0.25;
-      ghost.invincible = true;
-      setTimeout(function () { //Not the most elegant solution .. since this will be caled at every update during the 1,5 sec long ghost period .. 
-        //and aslo when killed create a error since 1,5 sec after kill-hit .. the this=ghost .. does not exesit anymore.
-        ghost.renderable.alpha = 1;
-        ghost.invincible = false;
-      }, 1500);
-      //The above is moved to game.hitPlayer since its more natural to ajust it from there .. not in the update loop.
+		var body = document.getElementsByTagName("body")[0];
+		body.appendChild(this.canvas);
 
+		this.ctx = this.canvas.getContext("2d");
+		this.ctx.drawImage(this.renderable.image, 0, 0);
 
-    }
-    */
+		if (this.data.vestimenta.pelo != "") {
+			img = me.loader.getImage(this.data.vestimenta.pelo);
+			this.ctx.drawImage(img, 0, 0);
+		}
+		if (this.data.vestimenta.pantalon != "") {
+			img = me.loader.getImage(this.data.vestimenta.pantalon);
+			this.ctx.drawImage(img, 0, 0);
+		}
+		if (this.data.vestimenta.remera != "") {
+			img = me.loader.getImage(this.data.vestimenta.remera);
+			this.ctx.drawImage(img, 0, 0);
+		}
 
-    if (me.input.isKeyPressed('shoot')) {
-      if (!this.isWeaponCoolDown && me.input.isKeyPressed('shoot')) {
-        this.isWeaponCoolDown = true;
-        var player = this;
+		this.renderable.image.src = this.canvas.toDataURL();
 
-        setTimeout(function () { player.isWeaponCoolDown = false; }, this.weaponCoolDownTime);
-        game.fireBullet({ x: this.pos.x + 12, y: this.pos.y + 12 }, game.mouseTarget, game.playerId, true);
-      }
-    }
-
-    this.stateChanged = false;
-    if (me.input.isKeyPressed('left')) {
-      this.animationToUseThisFrame = 'run-left';
-      this.vel.x -= 1; //Just set upp direction. calc actual vel later
-      this.state['left'] = true;
-      this.stateChanged = true;
-    } else {
-      this.state['left'] = false;
-    }
-
-    if (me.input.isKeyPressed('right')) {
-      this.animationToUseThisFrame = 'run-right';
-      this.vel.x += 1;
-      this.state['right'] = true;
-      this.stateChanged = true;
-    } else {
-      this.state['right'] = false;
-    }
-
-    if (me.input.isKeyPressed('up')) {
-      this.animationToUseThisFrame = 'run-up';
-      this.vel.y -= 1;
-      this.state['up'] = true;
-      this.stateChanged = true;
-    } else {
-      this.state['up'] = false;
-    }
-
-    if (me.input.isKeyPressed('down')) {
-      this.animationToUseThisFrame = 'run-down';
-      this.vel.y += 1;
-      this.state['down'] = true;
-      this.stateChanged = true;
-    } else {
-      this.state['down'] = false;
-    }
-    if (this.animationToUseThisFrame != this.lastAnimationUsed) {
-      this.lastAnimationUsed = this.animationToUseThisFrame;
-      this.renderable.setCurrentAnimation(this.animationToUseThisFrame);
-    }
-
-
-    //Now calc actual vel to prevent speeding by going diag.. 
-    this.vel.normalize();
-    this.vel.scale(this.accelForce * this.g_dt);
-    
-    this.updateMovement();
-
-    
-
-    //check for collisions
-    var res = me.game.world.collide(this);
-    if(res) {
-      if (res.obj.type == "solid") {
-        this.pos.x -= res.x;
-        this.pos.y -= res.y;
-        if (res.x) this.vel.x = 0; //Thse 2 lines not usefull in this implementation since we do not keep values any way .. but will be used if we do more acurate simulation in future.
-        if (res.y) this.vel.y = 0;
-      }
-    }
-
-    if (this.stateChanged) { //Only update animation and brodcast if we actualy need.
-      game.socket.emit('updatePlayerState', { x: this.pos.x, y: this.pos.y }, this.state);
-      this.parent(dt);
-      return true;
-    } else { 
-      return false;
-    }
-    
-    
-  }
+	}
 });
---
+++ public/js/play.js
@@ -0,0 +1,38 @@
+game.PlayScreen = me.ScreenObject.extend({
+
+	/**
+	 * action to perform on state change
+	 */
+	onResetEvent : function() {
+		// load a level
+		me.levelDirector.loadLevel("mapa");
+		// subscribe to key down event
+		me.audio.playTrack("snow", 0.7);
+
+		$.getJSON("api/player.json", function(data) {
+			game.mainPlayer = me.pool.pull('mainPlayer', Number(data.x),
+					Number(data.y), {
+						image : data.data.tipo,
+						spritewidth : 32,
+						spriteheight : 48,
+						width : 48,
+						height : 48,
+						id : data.id,
+						data : data.data
+					});
+			me.game.world.addChild(game.mainPlayer, 10);
+			game.players[data.id] = game.mainPlayer;
+			me.game.world.sort();
+
+			// start the game
+
+		});
+	},
+
+	/**
+	 * action to perform when leaving this screen (state change)
+	 */
+	onDestroyEvent : function() {
+		me.audio.stopTrack("snow");
+	}
+});
