d0ba96808268734f308a3c87222c03e5685fc561
Ricardo Martin Marcucci <rmarku@gmail.com>
2014-09-13 15:32:32 -0300

    Se acomoda el HTML de game

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,7 @@ config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/', {template: JST["assets/templates/index.html"]});
     $routeProvider.when('/crear-cuenta', {template: JST["assets/templates/crear-cuenta.html"], controller: 'AlumnoCreateCtl'});
     $routeProvider.when('/misiones', {template: JST["assets/templates/gmisiones.html"]});
-//    $routeProvider.when('/coliseo', {templateUrl: 'partials/user-creation.html', controller: 'UserCreationCtrl'});
+    $routeProvider.when('/game', {template: JST["assets/templates/game.html"]});
 //    $routeProvider.when('/personaje', {template: JST["assets/templates/personaje.html"], controller: 'AlumnoCreateCtl'});
 //    $routeProvider.when('/ranking', {templateUrl: 'partials/user-creation.html', controller: 'UserCreationCtrl'});
     $routeProvider.otherwise({redirectTo: '/'});
