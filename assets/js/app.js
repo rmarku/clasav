15c4cb7d785a0e9a063f2fdc75a963a8ec30dd0a
Agustina Edme Aliciardi <edmealiciardi@gmail.com>
2014-09-21 23:44:57 -0300

    Se fixeo la imagen de fondo y se comenzo a implementar minimap

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,10 @@
 //creamos nuestro modulo llamado app
-<<<<<<< HEAD
-var app = angular.module("juegoapl", ['juegoapl.services',  'juegoapl.controllers']).
-config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/', {template: JST["assets/templates/index.html"]});
-    $routeProvider.when('/crear-cuenta', {template: JST["assets/templates/crear-cuenta.html"], controller: 'AlumnoCreateCtl'});
-    $routeProvider.when('/misiones', {template: JST["assets/templates/gmisiones.html"]});
-    $routeProvider.when('/juego', {template: JST["assets/templates/game.html"]});
-//    $routeProvider.when('/coliseo', {templateUrl: 'partials/user-creation.html', controller: 'UserCreationCtrl'});
-=======
 var app = angular.module("juegoapl", ['juegoapl.services', 'juegoapl.controllers']).
     config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/', {template: JST["assets/templates/index.html"]});
         $routeProvider.when('/crear-cuenta', {template: JST["assets/templates/crear-cuenta.html"], controller: 'AlumnoCreateCtl'});
         $routeProvider.when('/misiones', {template: JST["assets/templates/gmisiones.html"]});
         $routeProvider.when('/game', {template: JST["assets/templates/game.html"], controller: 'gamePageController'});
->>>>>>> remotes/origin/developer/MarkU
 //    $routeProvider.when('/personaje', {template: JST["assets/templates/personaje.html"], controller: 'AlumnoCreateCtl'});
 //    $routeProvider.when('/ranking', {templateUrl: 'partials/user-creation.html', controller: 'UserCreationCtrl'});
         $routeProvider.otherwise({redirectTo: '/'});
@@ -29,5 +19,4 @@ var app = angular.module("juegoapl", ['juegoapl.services', 'juegoapl.controllers
  $routeProvider.when('/user-creation', {templateUrl: 'partials/user-creation.html', controller: 'UserCreationCtrl'});
  $routeProvider.otherwise({redirectTo: '/dummy'});
  }]);
-
  */
