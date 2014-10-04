0984b10a75d45e27f3a420d6854e4fafc09a052a
Agustina Edme Aliciardi <edmealiciardi@gmail.com>
2014-10-04 16:40:01 -0300

    Se agregaron los botones OVER del HUD

--- assets/js/hud.js
+++ assets/js/hud.js
@@ -1,19 +1,76 @@
-var sonido=1;
-
-function cambia_imagen() {
-	
-	if(sonido!=0){
-		document.getElementById('audio').src = "../images/iconos/audio_OFF.png";
-		me.audio.muteAll();
-		sonido=0;
-		return 0;
-	}
-	
-	if(sonido==0){
-		document.getElementById('audio').src = "../images/iconos/audio_ON.png";
-		me.audio.unmuteAll();
-		sonido=1;
-		return 0;
-	}
-	
+function noPressButton() {
+	$("#btnPers").attr("src", "../images/iconos/btn_personaje.png");
+	$("#btnInve").attr("src", "../images/iconos/btn_inventario.png");
+	$("#btnMisi").attr("src", "../images/iconos/btn_mision.png");
+	$("#btnLogr").attr("src", "../images/iconos/btn_logros.png");
+	$("#btnTale").attr("src", "../images/iconos/btn_talentos.png");
 }
+
+// Esto se ejecuta al terminar de cargar la pagina
+$(function() {
+	
+	//Habilitar o Deshabilitar audio
+	$("#audio").click(function() {
+		if (me.audio.getVolume() != 0) {
+			$("#audio").attr("src", "../images/iconos/audio_OFF.png");
+			me.audio.muteAll();
+		} else {
+			$("#audio").attr("src", "../images/iconos/audio_ON.png");
+			me.audio.unmuteAll();
+		}
+	});
+
+	/* 	BOTONERA DEL JUEGO	
+	 */
+	
+	//Boton para ver los items que tiene equipados la PERSONA
+	$("#btnPers").click(function() {
+		noPressButton();
+		if ($("#btnPers").attr("src") != "../images/iconos/btn_personajeOVER.png") {
+			$("#btnPers").attr("src", "../images/iconos/btn_personajeOVER.png");
+		} else {
+			$("#btnPers").attr("src", "../images/iconos/btn_personaje.png");
+		}
+	});
+
+	//Boton para ver el inventario de items
+	$("#btnInve").click(function() {
+		noPressButton();
+		if ($("#btnInve").attr("src") != "../images/iconos/btn_inventarioOVER.png") {
+			$("#btnInve").attr("src", "../images/iconos/btn_inventarioOVER.png");
+		} else {
+			$("#btnInve").attr("src", "../images/iconos/btn_inventario.png");
+		}
+	});
+	
+	//Boton para mostrar el listado de misiones
+	$("#btnMisi").click(function() {
+		noPressButton();
+		if ($("#btnMisi").attr("src") != "../images/iconos/btn_misionOVER.png") {
+			$("#btnMisi").attr("src", "../images/iconos/btn_misionOVER.png");
+		} else {
+			$("#btnMisi").attr("src", "../images/iconos/btn_mision.png");
+		}
+	});
+
+	//Boton para ver el listado de logros obtenidos
+	$("#btnLogr").click(function() {
+		noPressButton();
+		if ($("#btnLogr").attr("src") != "../images/iconos/btn_logrosOVER.png") {
+			$("#btnLogr").attr("src", "../images/iconos/btn_logrosOVER.png");
+		} else {
+			$("#btnLogr").attr("src", "../images/iconos/btn_logros.png");
+		}
+	});
+	
+	//Boton para ver los talentos	
+	$("#btnTale").click(function() {
+		noPressButton();
+		if ($("#btnTale").attr("src") != "../images/iconos/btn_talentosOVER.png") {
+			$("#btnTale").attr("src", "../images/iconos/btn_talentosOVER.png");
+		} else {
+			$("#btnTale").attr("src", "../images/iconos/btn_talentos.png");
+		}
+	});
+
+});
