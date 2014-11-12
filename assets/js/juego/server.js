dcbeed22df876be2a9b8a28efff9d3d7030f274a
fabriciocollino <fabricio.collino@gmail.com>
2014-11-12 13:32:15 -0300

    Multiplayer con update server de x e y en cambios de posicion

--- assets/js/juego/server.js
+++ assets/js/juego/server.js
@@ -5,7 +5,11 @@
 var server = {
 
     update_counter: 0,
-    update_timeOut: 100, // (3)segs aproximadamente
+    update_counterLimit: 1,
+    previous_position :{
+        x:0,
+        y:0
+    },
 
     /**
      * Description
@@ -14,37 +18,25 @@ var server = {
      * @param {} local_coordenates
      * @return
      */
-        update_mainplayer: function (local_coordenates) {
-        // Si hay algun estado activo (es decir si el jugador no esta quieto, y esta en movimiento), aumentar counter
-        if (game.mainPlayer.direccion !== 0) {
-            game.update_counter++;
-        }
 
-        //Envio al servidor solo si: se agoto el counter, o si hubo algun cambio de estado (respecto al ultimo cambio de estado)
-        if ((this.update_counter >= this.update_timeOut) || (game.mainPlayer.direccion != game.mainPlayer.direccion_anterior)) {
+     update_mainplayer: function (local_coordenates) {
+
+        //Envio al servidor solo si:  o si hubo algun cambio en la ultima posicion conocida
+        if ( (game.mainPlayer.pos.x != this.previous_position.x)|| (game.mainPlayer.pos.y != this.previous_position.y)) {
 
             io.socket.put('/api/personaje/updateStatus', {
                     mapa_instancia : game.mainPlayer.data.mapa_instancia.id.toString(),
                     id      :   game.mainPlayer.id,
-                    estado  :   game.mainPlayer.direccion,
+                    direccion  :   game.mainPlayer.direccion,
                     x       :   ~~game.mainPlayer.pos.x,
                     y       :   ~~game.mainPlayer.pos.y
                 }
                 , function (resdata) {
                 }
             );
-            /*
-            io.socket.put('/api/personaje/' + game.mainPlayer.id, {
-                    estado: game.mainPlayer.direccion,
-                    x: ~~game.mainPlayer.pos.x, y: ~~game.mainPlayer.pos.y
-                }
-                , function (resdata) {
-                }
-            );
-            */
 
-            game.mainPlayer.direccion_anterior = game.mainPlayer.direccion;
-            this.update_counter = 0;
+            this.previous_position.x = game.mainPlayer.pos.x;
+            this.previous_position.y = game.mainPlayer.pos.y;
         }
     },
 
@@ -71,11 +63,16 @@ var server = {
     },
 
     listen_events: function () {
+        var counter = 0;
 
         io.socket.on('otherPlayer_updateState', function messageReceived(obj) {
-            game.players[obj.id].direccion = obj.estado;
+
+
+            game.players[obj.id].direccion = obj.direccion;
             game.players[obj.id].pos.x = obj.x;
             game.players[obj.id].pos.y = obj.y;
+
+
             game.players[obj.id].updateBounds();
         });
 
