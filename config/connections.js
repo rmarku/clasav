4148d415156bd09b691c9faf0b2a921de43cba54
Ricardo Martin Marcucci <rmarku@gmail.com>
2014-11-14 08:39:45 -0300

    Se agrego configuracion MongoLABS y Redis de heroku, para permanencia de datos

--- config/connections.js
+++ config/connections.js
@@ -56,13 +56,13 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  someMongodbServer: {
+  MongoLab: {
     adapter: 'sails-mongo',
-    host: 'localhost',
-    port: 27017,
-    // user: 'username',
-    // password: 'password',
-    // database: 'your_mongo_db_name_here'
+    host: 'ds047800.mongolab.com',
+    port: 47800,
+    user: 'clasave',
+    password: '***REMOVED***',
+    database: 'clases-y-aventuras'
   },
 
   /***************************************************************************
