135abe47fddba0ab2935cfd7ca10bb8e9f4f1f16
Ricardo Martin Marcucci <rmarku@gmail.com>
2014-11-15 10:08:58 -0300

    Se coloca sourcemap en heroku

--- tasks/config/uglify.js
+++ tasks/config/uglify.js
@@ -12,10 +12,10 @@
 module.exports = function(grunt) {
 
 	grunt.config.set('uglify', {
-        //options:{
-        //    sourceMapIncludeSources: true,
-        //    sourceMap: true
-        //},
+        options:{
+            sourceMapIncludeSources: true,
+            sourceMap: true
+        },
 		dist: {
             src: ['.tmp/public/concat/production.js'],
             dest: '.tmp/public/min/production.min.js'
