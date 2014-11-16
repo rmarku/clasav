269d681e4c35406b8fe91c49b4b76ed48094039a
fabriciocollino <fabricio.collino@gmail.com>
2014-11-16 11:39:09 -0300

    prueba update Other Player en cambios de estado con timOut menor

--- assets/js/juego/entities/otherPlayer.js
+++ assets/js/juego/entities/otherPlayer.js
@@ -6,27 +6,6 @@ game.OtherPlayer = game.Player.extend({
     },
 
     update: function (dt) {
-        //this._super(game.Player, 'update', [dt]);
-        if (this.direccion & 1)
-            this.animationToUseThisFrame = "run-down";
-        else if (this.direccion & 2)
-            this.animationToUseThisFrame = "run-up";
-        else if (this.direccion & 4)
-            this.animationToUseThisFrame = "run-right";
-        else if (this.direccion & 8)
-            this.animationToUseThisFrame = "run-left";
-
-
-        if (this.lastAnimationUsed != this.animationToUseThisFrame) {
-            this.lastAnimationUsed = this.animationToUseThisFrame;
-            this.renderable.setCurrentAnimation(this.animationToUseThisFrame);
-        }
-        this.body.update();
-
-        if (this.direccion !== 0) {
-            this._super(me.Entity, 'update', [dt]);
-            return true;
-        }
-        return false;
+        this._super(game.Player, 'update', [dt]);
     }
 });
--- assets/js/juego/server.js
+++ assets/js/juego/server.js
@@ -5,11 +5,7 @@
 var server = {
 
     update_counter: 0,
-    update_counterLimit: 1,
-    previous_position :{
-        x:0,
-        y:0
-    },
+    update_timeOut: 30, // (1)segs aproximadamente
 
     /**
      * Description
@@ -18,25 +14,37 @@ var server = {
      * @param {} local_coordenates
      * @return
      */
+    update_mainplayer: function (local_coordenates) {
+        // Si hay algun estado activo (es decir si el jugador no esta quieto, y esta en movimiento), aumentar counter
+        if (game.mainPlayer.direccion !== 0) {
+            game.update_counter++;
+        }
 
-     update_mainplayer: function (local_coordenates) {
-
-        //Envio al servidor solo si:  o si hubo algun cambio en la ultima posicion conocida
-        if ( (game.mainPlayer.pos.x != this.previous_position.x)|| (game.mainPlayer.pos.y != this.previous_position.y)) {
+        //Envio al servidor solo si: se agoto el counter, o si hubo algun cambio de estado (respecto al ultimo cambio de estado)
+        if ((this.update_counter >= this.update_timeOut) || (game.mainPlayer.direccion != game.mainPlayer.direccion_anterior)) {
 
             io.socket.put('/api/personaje/updateStatus', {
                     mapa_instancia : game.mainPlayer.data.mapa_instancia.id.toString(),
                     id      :   game.mainPlayer.id,
-                    direccion  :   game.mainPlayer.direccion,
+                    estado  :   game.mainPlayer.direccion,
                     x       :   ~~game.mainPlayer.pos.x,
                     y       :   ~~game.mainPlayer.pos.y
                 }
                 , function (resdata) {
                 }
             );
+            /*
+             io.socket.put('/api/personaje/' + game.mainPlayer.id, {
+             estado: game.mainPlayer.direccion,
+             x: ~~game.mainPlayer.pos.x, y: ~~game.mainPlayer.pos.y
+             }
+             , function (resdata) {
+             }
+             );
+             */
 
-            this.previous_position.x = game.mainPlayer.pos.x;
-            this.previous_position.y = game.mainPlayer.pos.y;
+            game.mainPlayer.direccion_anterior = game.mainPlayer.direccion;
+            this.update_counter = 0;
         }
     },
 
@@ -63,53 +71,14 @@ var server = {
     },
 
     listen_events: function () {
-        var counter = 0;
 
         io.socket.on('otherPlayer_updateState', function messageReceived(obj) {
+            game.players[obj.id].direccion = obj.estado;
 
-
-            game.players[obj.id].direccion = obj.direccion;
-            game.players[obj.id].pos.x = obj.x;
-            game.players[obj.id].pos.y = obj.y;
-
+            //game.players[obj.id].pos.x = obj.x;
+            //game.players[obj.id].pos.y = obj.y;
 
             game.players[obj.id].updateBounds();
         });
-
-/*
-        // Listen to incoming Updates from Jugador_en_vivo we've just subscribed to
-        io.socket.on('personaje', function messageReceived(obj) {
-            if (obj.id != game.mainPlayer.id)
-                switch (obj.verb) {
-                    case 'updated':
-                        if ((obj.data.conectado == 'conectado') && (typeof game.playersOffline[obj.data.id] != 'undefined') ){
-                            game.saveOnlineOtherPlayer(game.playersOffline[id]);
-                            game.removeOfflineOtherPlayer(obj.data.id);
-                            me.game.world.addChild(game.players[obj.data.id], 9);
-                        }else
-                        if ((obj.data.conectado == 'desconectado') && (typeof game.players[obj.data.id] != 'undefined') ){
-                            game.saveOfflineOtherPlayer(game.players[id]);
-                            game.removeOtherPlayer(obj.data.id);
-                        }
-                        if (typeof obj.data.estado != 'undefined'){ //&& obj.data.estado != game.players[obj.id].direccion) {
-                            game.players[obj.id].direccion = obj.data.estado;
-                            //game.players[obj.id].direccion_anterior = game.players[obj.id].direccion;
-                        }
-                        if (typeof obj.data.x != 'undefined'){// && obj.data.x != ~~game.players[obj.id].x) {
-                            game.players[obj.id].pos.x = obj.data.x;
-                        }
-                        if (typeof obj.data.y != 'undefined'){// && obj.data.y != ~~game.players[obj.id].y) {
-                            game.players[obj.id].pos.y = obj.data.y;
-                        }
-                        game.players[obj.id].updateBounds();
-                        break;
-
-                    case 'created':
-                        break;
-                    default:
-                        break;
-                }
-        });
-*/
     }
 };
