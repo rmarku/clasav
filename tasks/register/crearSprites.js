7110b9d5f7e7945ea22f6fb43d1f480d175b9446
Ricardo Martin Marcucci <rmarku@gmail.com>
2014-12-13 10:07:23 -0300

    Agregado tambien ImageMagick a test

--- tasks/register/crearSprites.js
+++ tasks/register/crearSprites.js
@@ -7,7 +7,7 @@ module.exports = function (grunt) {
         var fs = require('fs');
 
         var gmagick;
-        if (process.env.NODE_ENV == 'production')
+        if (process.env.NODE_ENV == 'production' || process.env.NODE_ENV == 'test')
             gmagick = require('gm').subClass({imageMagick: true});
         else
             gmagick = require('gm');
