b395a0f426ced32c82a9274a2e01d5d4d6e82a11
fabriciocollino <fabricio.collino@gmail.com>
2015-04-23 21:45:03 -0300

    ..

--- api/controllers/ClaseController.js
+++ api/controllers/ClaseController.js
@@ -192,7 +192,7 @@ module.exports = {
                     User.findOne({id:userID}).populate('clase_x_user',{clase:clase.id}).exec(function afterwards(err,user) {
 
                         if (err || !user) {
-                            console.log(err);F
+                            console.log(err);
                             res.json(err);
                             return;
                         }
--- assets/js/juego/main.js
+++ assets/js/juego/main.js
@@ -407,6 +407,7 @@ var game = {
                                     'data/minijuegos/' +
                                     data.mision.substring(4) +
                                     '"></iframe>');
+                                    $("#mision_cancelar").show();
 
                                 } else {
                                     $("#mision_txt").html(data.mision);
@@ -479,8 +480,7 @@ var game = {
                 // Doy de baja el evento de la tecla
                 me.input.triggerKeyEvent(me.input.KEY.SPACE, false);
             }
-        }
-        ,
+        },
 
         get_misClases: function () {
             $.get('/api/clase/get_misClases', function (clases) {
