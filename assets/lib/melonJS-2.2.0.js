2c1562cea77f2ab4bffbf6d5cbe8114f963e3307
Ricardo Martin Marcucci <rmarku@gmail.com>
2015-05-01 22:31:06 -0300

    * added newrelic browser * arreglado melon para que no tener scrollbars

--- assets/lib/melonJS-2.2.0.js
+++ assets/lib/melonJS-2.2.0.js
@@ -12671,8 +12671,8 @@ THE SOFTWARE.
                     sWidth = Math.min(maxWidth, designHeight * screenRatio);
                     scaleX = scaleY = _max_width / sWidth;
                     sWidth = ~~(sWidth + 0.5);
-                    this.renderer.resize(sWidth, designHeight);
-                    me.game.viewport.resize(sWidth, designHeight);
+                    this.renderer.resize(sWidth-5, designHeight-5);
+                    me.game.viewport.resize(sWidth-5, designHeight-5);
                     /*
                      * XXX: Workaround for not updating container child-bounds
                      * automatically (it's expensive!)
